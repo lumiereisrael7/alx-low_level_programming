5d29e3f9f382963dc108f4bc0ef68abe36904e53
Lumiere Israel <lumiereisrael7@gmail.com>
2022-12-09 15:50:47 -0800

    second exercice

---
+++ 1-last_digit.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+	int d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	
+	d = n%10;
+	printf("Last digit of %d is %d and is", n, d);
+	if (d == 0)
+		printf(" 0\n");
+	else if (d < 6 && d != 0)
+		printf(" less than 6 and not 0\n");
+	else if (d > 5)
+		printf(" greater than 5\n");
+	return (0);
+}
